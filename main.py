2029bf9fa0c7f9b523532809be89f0ada55bc748
movags95 <45894549+movags95@users.noreply.github.com>
2022-02-07 21:45:42 +0000

    First commit

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
-def getdata():
-    pass
+from ast import parse
+import backtrader as bt
+import pandas as pd
+import argparse
+from strategies import TestStrategy
 
+def getdata(csvname):
+
+    return bt.feeds.YahooFinanceCSVData(dataname=csvname)
+
+def main(csvname, strategy=None, startingcash=10000, commission=None, sizertype='Percent', sizeramt=0.95):
+    data = getdata(csvname)
+    cerebro = bt.Cerebro()
+    cerebro.broker.setcommission(commission=commission) if commission else None
+    cerebro.broker.setcash(startingcash)
+    
+    if data is not None:
+        cerebro.adddata(data)
+        cerebro.addsizer(bt.sizers.PercentSizer, percents=sizeramt) if sizeramt and sizertype == 'Percent' else cerebro.addsizer(bt.sizers.FixedSize, stake=sizeramt)
+    
+    cerebro.addstrategy(strategy) if strategy is not None else None
+    
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    cerebro.run()
+
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    cerebro.plot()
+
+if __name__ == '__main__':
+   main('AAPL.csv', TestStrategy)
+   
---
+++ strategies.py
@@ -0,0 +1,57 @@
+import backtrader as bt
+
+# Create a Stratey
+class TestStrategy(bt.Strategy):
+
+    def log(self, txt, dt=None):
+        ''' Logging function for this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+    
+    def __init__(self):
+        self.dataclose = self.datas[0].close
+        self.order = None
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log('BUY EXECUTED, %.2f' % order.executed.price)
+            elif order.issell():
+                self.log('SELL EXECUTED, %.2f' % order.executed.price)
+
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+        
+        print(order)
+
+        # Write down: no pending order
+        self.order = None
+
+    def next(self):
+        self.log('Close, %.2f' % self.dataclose[0])
+
+        if self.order:
+            return
+
+        if not self.position:
+            if self.dataclose[0] < self.dataclose[-1]:
+                if self.dataclose[-1] < self.dataclose[-2]:
+                    self.log('BUY, %.2f' % self.dataclose[0])
+                    self.buy()
+
+
+        else:
+            if len(self) >= (self.bar_executed + 5):
+                # SELL, SELL, SELL!!! (with all possible default parameters)
+                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.sell()
